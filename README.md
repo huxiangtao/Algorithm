a11a132933f9b8dc2b18833dd181432c51580b6c
Huxiangtao <elliot130@163.com>
2017-04-17 23:22:29 +0800

    add

--- README.md
+++ README.md
@@ -11,3 +11,16 @@ part of my daily plan for studying C
 ```
 gets()代表get string,它从系统的标准输入设备获得一个字符串。它读取换行符之前的所有字符，在这些字符后添加一个空字符(\0)，然后把这个字符串交给调用它的程序。
 注意：gets()返回的指针与传给它的是同一个指针。
+
+###typedef
+```c
+typedef struct person {
+	char * name;
+	int age;
+}man,*people;
+```
+person作为该结构的标记名，typedef将该结构声明为man类型和(*people)类型。可以简化成：
+```
+	man * people;
+```
+people就是一个指向man类型的指针。
--- binaryTree/binaryTree.c
+++ binaryTree/binaryTree.c
@@ -1,52 +1,21 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-typedef struct binaryItem {
-	int value;
-	struct binaryItem * left;
-	struct binaryItem * right;
-} NODE;
+typedef struct binaryTree {
+	int data;
+	struct binaryTree * left;
+	struct binartTree * right;
+}binaryNode;
 
-void initTree(NODE *,int);
-void insertNode(NODE *,int);
+void createBinTree(binaryNode *);
 
-int main(void) {
-	NODE root;
+void createBinTree(binaryNode * node) {
 	int n;
-	printf("Please enter a number to init the tree root.\n");
 	scanf("%d",&n);
-	initTree(&root,n);	
-	printf("Please insert node now.\n");
-	scanf("%d",&n);	
-	insertNode(&root,n);
-	printf("Please enter next node.\n");
-	while(1) {
-		scanf("%d",&n);
-		if(root.left != NULL)
-			insertNode(root.left,n);
-		else if(root.right != NULL)
-			insertNode(root.right,n);
-		printf("Please enter next node.\n");
-	}
-	return 0;
+	node = (binaryTree *)malloc(sizeof(binaryNode));
+	node->data = n;
+	createBinTree(node->left);
+	createBinTree(node->right);
 }
 
-void initTree(NODE * item,int n) {
-	item->value = n;
-	item->left = NULL;
-	item->right = NULL;
-}
 
-void insertNode(NODE * item,int n) {
-	NODE node = {
-		n,
-		NULL,
-		NULL
-	};
-
-	if(node.value > item->value)
-		item->right = &node;
-	else if(node.value == item->value)
-		return;
-	else
-		item->left = &node;
-}
